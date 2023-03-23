0fbd46e3dc28c86895510ee3504030db99166505
Rafael Barroso <76064049+Raafa81@users.noreply.github.com>
2023-03-23 16:43:55 -0300

    Resposta

---
+++ resposta.ts
@@ -0,0 +1,38 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros?
+let employee = {code : 20, name : "Jhon"}
+console.log(employee)
+
+
+// Como podemos melhorar o esse código usando TS?
+
+interface pessoa{
+    nome: string,
+    idade: number,
+    profissao: string
+}
+
+let pessoa1 = {
+    nome: "maria",
+    idade :29,
+    profissao : "atriz"
+    };
+
+let pessoa2 = {
+    nome : "roberto",
+    idade :19,
+    profissao:"Padeiro"
+    };
+
+let pessoa3 = {
+    nome: "laura",  
+    idade: "32",
+    profissao: "Atriz"
+    };
+
+
+
+let pessoa4 = {
+    nome:"carlos",
+    idade : 19,
+    profissao : "padeiro"
+    }
